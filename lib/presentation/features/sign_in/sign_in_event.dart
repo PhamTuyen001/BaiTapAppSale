c1413584a30ee09ba16ebdba477c9bb3ca91ccb4
PhamTuyen <phamquangtuyen001@gmail.com>
2022-12-15 23:49:37 +0700

    SignIn Event - Bloc

---
+++ lib/presentation/features/sign_in/sign_in_event.dart
@@ -0,0 +1,24 @@
+import '../../../common/bases/base_event.dart';
+
+class SignInEvent extends BaseEvent {
+  String email, password;
+  SignInEvent({required this.email, required this.password});
+  @override
+  List<Object?> get props => [];
+
+}
+
+class SignInSuccessEvent extends BaseEvent {
+  @override
+  List<Object?> get props => [];
+
+}
+
+class SignInFailEvent extends BaseEvent {
+  String message;
+
+  SignInFailEvent({required this.message});
+  @override
+  List<Object?> get props => [];
+
+}
